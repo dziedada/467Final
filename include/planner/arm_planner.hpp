268263637927eacf6d6e5a0cc26833cbf07e1f07
Jonas Hirshland <jhirshey@umich.edu>
2019-12-08 22:12:43 -0800

    don't publish plan unless have a best ball

--- include/planner/arm_planner.hpp
+++ include/planner/arm_planner.hpp
@@ -151,8 +151,11 @@ class ArmPlanner
                 }
             }
 			outer_loop_controller.update_target( bestBall->reachPrediction );
-			std::cout << "position: " << bestBall->reachPrediction.ball_inrange_position_ << std::endl;
-			publishPlan( bestBall->reachPrediction.ball_inrange_position_ );
+			if ( bestBall )
+				{
+				std::cout << "position: " << bestBall->reachPrediction.ball_inrange_position_ << std::endl;
+				publishPlan( bestBall->reachPrediction.ball_inrange_position_ );
+				}
 
             std::cout << "corresponded: " << balls.size() << std::endl;
             cond_var->notify_all();
