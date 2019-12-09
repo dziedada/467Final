44f2a1c9a1561113c143230f52fb9d8964884bba
Ryan Wunderly <rywunder@umich.edu>
2019-12-09 00:16:44 -0500

    add line of best fit to coords

--- include/planner/ball.hpp
+++ include/planner/ball.hpp
@@ -22,6 +22,7 @@
 #include <list>
 #include <deque>
 #include <chrono>
+#include <queue>
 
 using cv::Mat;
 
@@ -39,14 +40,17 @@ class Ball
 		int64_t utime; // previous utime
         int odds; // odds we think this ball is fake news
         // int64_t inputTime; // EKF system utime
+        cv::Mat meas;  // [x, y]
 		Vector2d coordinate;					// ( x, y )
+		Vector2d prevDetectionPos;
 		Vector2d velocity;					// ( v_x, v_y )
 		Vector2d coordinatePrediction;	// ( x_1, y_2 )
-		cv::KalmanFilter kf;
-		cv::Mat state; // [x, y, v_x, v_y]
-		cv::Mat meas;  // [x, y]
-		std::vector<double> kf_error_history;
-		std::list< Vector2d > velocityHistory;//( 5, Vector2d( 0.0, 0.0 ), std::deque<Vector2d>);
+
+		std::vector<double> coordLine; // [m, b]
+
+		std::deque< Vector2d > coordHistory;
+		std::deque<double> speedHistory;
+		std::list< Vector2d > velocityHistory;
 		const int HISTORY_SIZE = 10;
 		Prediction reachPrediction;
         friend class ArmPlanner;
@@ -57,76 +61,60 @@ class Ball
 				: id( id_ ), color( col ), utime( time ), odds( 0 ),
                 coordinate( coord ), velocity( vel )
 		{
-			// initialize a kalman filter
-			int stateSize = 4; // [x, y, v_x, v_y]
-			int measSize = 4;  // [x, y]
-			int contrSize = 0; // no control input
-			unsigned int type = CV_64F; // double type
+			int measSize = 2;  // [x, y]
+			meas = Mat(measSize, 1, CV_64F);
 
-			kf = cv::KalmanFilter(stateSize, measSize, contrSize, type);
+			meas.at<double>(0) = coordinate.x();
+            meas.at<double>(1) = coordinate.y();
 
-			state = Mat(stateSize, 1, type);  // [x,y,v_x,v_y]
-    		meas = Mat(measSize, 1, type);    // [z_x,z_y,z_vx,z_vy]
-
-			// Transition matrix
-		    // Note: set dT at each processing step!
-		    // [ 1 0 dT 0  ]
-		    // [ 0 1 0  dT ]
-		    // [ 0 0 1  0  ]
-		    // [ 0 0 0  1  ]
-			cv::setIdentity(kf.transitionMatrix);
-
-			// Measure Matrix H
-		    // [ 1 0 0 0]
-		    // [ 0 1 0 0]
-		    kf.measurementMatrix = Mat::zeros(measSize, stateSize, type);
-		    kf.measurementMatrix.at<double>(0) = 1.0;
-		    kf.measurementMatrix.at<double>(5) = 1.0;
-		    kf.measurementMatrix.at<double>(10) = 1.0;
-		    kf.measurementMatrix.at<double>(15) = 1.0;
-
-		    // Process noise covariance matrix Q
-		    // TODO: Tune
-
-		    /*kf.processNoiseCov.at<double>(0) = 1e-2;
-		    kf.processNoiseCov.at<double>(5) = 1e-2;
-		    kf.processNoiseCov.at<double>(10) = 1e-5;
-		    kf.processNoiseCov.at<double>(15) = 1e-5;*/
-		    cv::setIdentity(kf.processNoiseCov, cv::Scalar(1));
-		    kf.processNoiseCov.at<double>(10) = 1;
-		    kf.processNoiseCov.at<double>(15) = 1;
-
-		    // Measures Noise Covariance Matrix R
-    		cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1));
-		    //kf.processNoiseCov.at<double>(10) = 1;
-		    //kf.processNoiseCov.at<double>(15) = 1;
-
-    		// First detection!
-            // >>>> Initialization of Prior
-            std::cout << "First detection: " << coord << std::endl;
-    		cv::setIdentity(kf.errorCovPre, cv::Scalar(1e-2));
-            /*kf.errorCovPre.at<double>(0) = 1e-2; // 1 cm
-            kf.errorCovPre.at<double>(5) = 1e-2; // 1 cm*/
-            kf.errorCovPre.at<double>(10) = 1e-1;
-            kf.errorCovPre.at<double>(15) = 1e-1;
-
-            state.at<double>(0) = coord.x();
-            state.at<double>(1) = coord.y();
-            state.at<double>(2) = vel.x();
-            state.at<double>(3) = vel.y();
-            // <<<< Initialization
-
-            kf.statePost = state;    
+			// Initialize coord history   
 			for ( int i = 0; i < HISTORY_SIZE; ++i )
-				{
+			{
+				coordHistory.push_back(coord);
+			}
+
+			for ( int i = 0; i < HISTORY_SIZE; ++i )
+			{
+				speedHistory.push_back(vel.norm());
+			}
+
+			for ( int i = 0; i < HISTORY_SIZE; ++i )
+			{
 				velocityHistory.push_back( Vector2d( 0.0, 0.0 ) );
-				}
 
 			reachPrediction.ball_in_range_time_ = 1000;
 			reachPrediction.ball_inrange_position_ = Vector2d( 0.0, 0.0 );
 			reachPrediction.ball_inrange_velocity_ = Vector2d( 0.0, 0.0 );
 			reachPrediction.goal_ = Vector2d( 0.0, 0.0 );
 			reachPrediction.utime_ = 0;
+			}
+		}
+
+		std::vector<double> GetLinearFit(const std::deque<Vector2d>& data)
+		{
+		    double xSum = 0, ySum = 0, xxSum = 0, xySum = 0, slope, intercept;
+		    for (long i = 0; i < data.size(); i++)
+		    {
+		        xSum += data[i].x();
+		        ySum += data[i].y();
+		        xxSum += data[i].x() * data[i].x();
+		        xySum += data[i].x() * data[i].y();
+		    }
+		    slope = (data.size() * xySum - xSum * ySum) / (data.size() * xxSum - xSum * xSum);
+		    intercept = (ySum - slope * xSum) / data.size();
+		    std::vector<double> res;
+		    res.push_back(slope);
+		    res.push_back(intercept);
+		    return res;
+		}
+
+		// projects point onto the line
+		Vector2d projectPt(Vector2d pt, double slope, double intercept)
+		{
+			//https://en.wikibooks.org/wiki/Linear_Algebra/Orthogonal_Projection_Onto_a_Line
+			pt.y() = pt.y() - intercept;
+			Vector2d lineV(1, slope);
+			return ( pt.dot(lineV) / (lineV.dot(lineV)) ) * lineV;
 		}
 
 		void update(const ball_detection_t &detection)
@@ -134,41 +122,28 @@ class Ball
             odds += 1;
 			double dT = (double)(detection.utime - utime) / (double)1000000;
 
-            velocity = Vector2d( ( detection.position[0] - coordinate[0] ) / dT, ( detection.position[1] - coordinate[1] ) / dT );
-            coordinate = Vector2d( detection.position[0], detection.position[1] );
-			
-			velocityHistory.pop_front();
+			// update measurement for gui
+			meas.at<double>(0) = coordinate.x();
+            meas.at<double>(1) = coordinate.y();
+
+            velocity = Vector2d( ( detection.position[0] - prevDetectionPos[0] ) / dT, ( detection.position[1] - prevDetectionPos[1] ) / dT );
+            Vector2d detectionPos = Vector2d( detection.position[0], detection.position[1] );
+            prevDetectionPos = detectionPos;
+
+            velocityHistory.pop_front();
 			velocityHistory.push_back( velocity );
+
+			coordHistory.pop_front();
+			coordHistory.push_back(detectionPos);
+
+			// speedHistory.pop_front();
+			// speedHistory.push_back((coordHistory.back() - coordHistory.back()-1).norm()/dT);
+			// use a line of best fit to get the coordinate prediction
+			coordLine = GetLinearFit(coordHistory);
+			coordinate = projectPt(detectionPos, coordLine[0], coordLine[1]);
 
 			utime = detection.utime;
             std::cout << "utime " << utime << " dT " << dT << std::endl;
-
-			// update Matrix A for correct velocity estimate 
-			kf.transitionMatrix.at<double>(2) = dT;
-            kf.transitionMatrix.at<double>(7) = dT;
-
-            // for error checking
-            Mat prediction = kf.predict();
-            coordinatePrediction = Vector2d(prediction.at<double>(0),prediction.at<double>(1));
-
-            // state estimate
-            meas.at<double>(0) = coordinate.x();
-            meas.at<double>(1) = coordinate.y();
-            meas.at<double>(2) = velocity.x();
-            meas.at<double>(3) = velocity.y();
-
-            //state = kf.predict();
-            state = kf.correct(meas);
-            Vector2d estimatePt(state.at<double>(0),state.at<double>(1));
-            Vector2d estimateVel(state.at<double>(2),state.at<double>(3));
-            /*std::cout << "ball: " << coordinate.x() << ", " << coordinate.y() << std::endl;
-            std::cout << "meas: " << detection.position[0] << " " << detection.position[1] << std::endl;
-            std::cout << "predictPos:  " << coordinatePrediction << std::endl;
-            std::cout << "updatepos: " << estimatePt << std::endl;
-            std::cout << "updateVel: " << estimateVel << std::endl;
-            std::cout << "DT: " << dT << std::endl;*/
-            kf_error_history.push_back( (estimatePt - coordinatePrediction).norm() );
-            //std::cout << "error: " << (estimatePt - coordinatePrediction).norm() << std::endl;
 		}
 		
 		void updatePrediction( const double outerRadius )
@@ -195,6 +170,8 @@ class Ball
 			return this->id == other.id;
 		}
 
+		// Check out realsense utime now
+
         Ball operator=( const Ball &other )
         {
 	        this->id = other.id;
@@ -217,19 +194,12 @@ class Ball
         // TODO refactor to predict state
 		Vector4d predict_coordinate( double dT)
 		{
-			// EKF Estimation
-			// update Matrix A for correct velocity estimate 
-			kf.transitionMatrix.at<double>(2) = dT;
-            kf.transitionMatrix.at<double>(7) = dT;
-            auto predState = kf.predict();
-			
 			// Point History Estimation ( using last 5 points )
 			Vector2d velocityEstimate( 0.0, 0.0 );
 			for ( auto &it: velocityHistory )
 				{
 				velocityEstimate.x() += it.x();
 				velocityEstimate.y() += it.y();
-				//std::cout << "vel est: " << velocityEstimate << std::endl;
 				}
 			velocityEstimate /= (double)HISTORY_SIZE;
 			//std::cout << "vel est final: " << velocityEstimate << std::endl;
@@ -269,11 +239,11 @@ class Ball
 
 		Vector2d getPos()
 		{
-			return Vector2d(state.at<double>(0), state.at<double>(1));
+			return coordinate;
 		}
 
 		Vector2d getVel()
 		{
-			return Vector2d(state.at<double>(2), state.at<double>(3));
+			return velocity;
 		}
 	};
